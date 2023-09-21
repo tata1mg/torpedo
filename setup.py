49e2738ba2573cf2f4cee4948b0e8fb7bb19116e
prshant70 <prashant.mishra@1mg.com>
2023-09-21 15:04:45 +0530

    Rmoved cqa dependency - it will be added again in the future releases (#2)

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ with open("requirements/base.txt") as f:
 with open("requirements/test.txt") as f:
     test_requirements = f.read().splitlines()
 
-with open("requirements/dev.txt") as f:
-    dev_requirements = f.read().splitlines()
-
-
 setup(
     name="torpedo",
     version="1.0.0",
@@ -18,5 +14,5 @@ setup(
     url="https://github.com/tata1mg/torpedo",
     description="Vanilla code for Sanic application setup",
     packages=find_packages(exclude=("examples", "requirements")),
-    install_requires=base_requirements + test_requirements + dev_requirements,
+    install_requires=base_requirements + test_requirements
 )
